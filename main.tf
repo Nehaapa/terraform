0d7c429bf494af2e822bbf543e8478915790ea8e
Nehaapa <159808175+Nehaapa@users.noreply.github.com>
2025-07-15 20:57:17 -0500

    terraform code

---
+++ main.tf
@@ -0,0 +1,62 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "myTFResourceGroup"
+    storage_account_name = "tfstate9lw1m"
+    container_name = "tfstate"
+    key = "dev.terraform.tfstate"
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "westus2"
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team        = "DevOps"
+    Company     = "Neha and Company"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                            = "tfstate${random_string.resource_code.result}"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  allow_nested_items_to_be_public = false
+  tags = {
+    environment = "development"
+  }
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
+
+resource "random_string" "resource_code" {
+  length  = 5
+  special = false
+  upper   = false
+}
